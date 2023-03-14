55e34729f9cca38f6859a1828de1837a5ef00a5a
Anthony Antonelli <anthony.anthony179@gmail.com>
2023-03-14 14:23:37 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task !== "") {
+      setTasks([...tasks, task]);
+    }
+  };
+
+  const handleCheck = (e) => {
+    const li = e.target.parentNode;
+
+    if (li.classList.contains("completed")) {
+      li.classList.remove("completed");
+    } else {
+      li.classList.add("completed");
+    }
+  };
+
+  const handleRemove = (e, index) => {
+    const li = e.target.parentNode;
+    li.remove();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="centerContent">
+        <h1>TO-DO List</h1>
+        <div id="todoScreen">
+          <div id="formDiv">
+            <form onSubmit={handleSubmit}>
+              <div id="cu">
+                <label htmlFor="objective">
+                  <input
+                    placeholder="Oque temos para hoje?"
+                    type="text"
+                    onChange={(e) => setTask(e.target.value)}
+                    
+                  />
+                <button id="addButton">Adicionar</button>
+                </label>
+              </div>
+            </form>
+          </div>
+          <div id="list">
+            <ul>
+              {tasks.map((todo, index) => (
+                <li key={index} className="tasks">
+                  <input
+                    type="checkbox"
+                    name="checkbox"
+                    onChange={(e) => handleCheck(e)}
+                  />
+                  <p>{todo}</p>
+                  <button className="removeButton" onClick={(e) => handleRemove(e)}>X</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
